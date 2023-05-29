7c82d43c5bd2b64d5a51289a9c10922125974196
chihebchabou <chihebchaboutech@gmail.com>
2023-05-29 18:02:38 +0100

    Create contact service

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -22,7 +22,7 @@ const Dashboard = () => {
     user && (
       <div className="container mt-5">
         <h5>
-          Welcome <span className="text-primary">John Doe</span>
+          Welcome <span className="text-primary">{user.name}</span>
         </h5>
         <div className="row">
           <div className="col-md-6">
